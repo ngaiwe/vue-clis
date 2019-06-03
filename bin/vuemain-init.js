5d37122f80cfee3138499d463c7ff82dceb287ae
weiran <weiran@vipkid.com.cn>
2019-06-03 11:17:04 +0800

    脚手架脚本

---
+++ bin/vuemain-init.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
+const chalk = require('chalk')
+const logSymbols = require('log-symbols')
+const download = require('../lib/download.js')
+const inquirer = require('../lib/inquirer.js')
+const generator = require('../lib/generator.js')
+
+program.usage('<project-name>').parse(process.argv)
+
+let projectName = program.args[0]
+let next = undefined
+
+if (!projectName) {
+  program.help()
+  return false
+}
+
+const list = glob.sync('*')
+let rootName = path.basename(process.cwd())
+
+if (list.length) {
+  let length = list.filter(name => {
+    const fileName = path.resolve(process.cwd(), path.join('.', name))
+    const isDir = fs.stat(fileName).isDirectory()
+    return name.indexOf(projectName) !== -1 && isDir
+  })
+  if (Boolean(length)) {
+    console.log(`项目${projectName}已经存在`)
+  } else {
+    next = Promise.resolve(projectName)
+  }
+} else if (rootName === projectName) {
+  next = inquirer.prompt([{
+    name: 'rootName',
+    message: '当前目录为空，且目录名称和项目名称相同，是否直接在当前目录下创建新项目？',
+    type: 'confirm',
+    default: true
+  }]).then(answer => {
+    return Promise.resolve(answer.rootName ? '.' : projectName)
+  })
+} else {
+  next = Promise.resolve(projectName)
+}
+
+next && go()
+
+function go() {
+  next.then(projectRoot => {
+    if (projectRoot == '.') {
+      void null
+    } else {
+      fs.mkdirSync(projectRoot)
+    }
+    return download(projectRoot).then(target => ({
+      projectRoot,
+      downloadTemp: target
+    }))
+  }).then(context => {
+    return inquirer().then(answers => {
+      return {
+        ...context,
+        metadata: {
+          ...answers
+        }
+      }
+    }).catch(err => {
+      return Promise.reject(err)
+    })
+  }).then(context => {
+    let { metadata, projectRoot, downloadTemp } = context
+    return generator(metadata, projectRoot, downloadTemp)
+  }).then(context => {
+    console.log('......')
+    console.log(logSymbols.success, chalk.green('创建成功:'))
+    console.log(chalk.blue(`进入项目目录：cd ${context}`))
+    console.log(chalk.blue(`安装依赖: yarn setup`))
+    console.log(chalk.blue(`启动项目: yarn dev`))
+  }).catch(err => {
+    console.error(logSymbols.error, chalk.red(`创建失败：${err}`))
+  })
+}
