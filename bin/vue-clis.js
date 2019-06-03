76748c03bc3822881b100b7963d6a0c1885c1916
weiran <weiran@vipkid.com.cn>
2019-06-03 20:17:45 +0800

    修改配置

--- bin/vue-clis.js
+++ bin/vue-clis.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
+const path = require('path')
 const program = require('commander')
 const config = require('../package.json')
 
 program
 	.version(config.version, '-V, --version')
-	.usage('<command> [项目名称]')
-	.command('init')
-	.description('项目初始化')
+	.command('init [name]', '初始化脚手架')
 
 program.parse(process.argv)
